65f346f73dc260633f74a8188170c34bc36f1cde
prithvi <prithvikolla8@gmail.com>
2020-10-06 18:53:08 -0500

    update new project

---
+++ GeektasticCodeChallenge_updated version 21 11 18/answers.sql
@@ -0,0 +1,18 @@
+-- PLEASE PASTE YOUR SQL IN THE SECTIONS BELOW
+
+-- 1. How much revenue (in GBP) and how many spenders did each game have for each day?
+
+
+
+
+-- 2. How many of each item was sold on each day, and how much revenue (in GBP) did each item generate on each day?
+
+
+
+
+-- 3. What items do users buy first, after having converted real money to GeekCoins for the first time? Answer for each user, including the time and amount of their first conversion of real money into GeekCoins, and also the time and which item was purchased first after this transaction.
+
+
+Notes
+For each question please provide a single executable query that shows the relevant values for all available data.
+
